b1aa73c949bde2ba2f0371055149b82113ef2723
Dea <dea.bocina9@gmail.com>
2024-12-01 01:03:14 +0100

    Integrate Tailwind, add i18n support (en/hr), add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import "./App.css";
+import { icons } from "./assets/assets";
+import "./config/i18n";
+import { useTranslation } from "react-i18next";
 
-export default App
+const App = () => {
+  const { t, i18n } = useTranslation();
+
+  const switchLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
+  return (
+    <div>
+      <header className="p-4 flex justify-between items-center">
+        <div className="logo size-[150px]">
+          <img src={icons.logo} alt="Logo" />
+        </div>
+        <div className="language-switch flex gap-3 mr-10">
+          <img
+            src={icons.en}
+            alt="English"
+            onClick={() => switchLanguage("en")}
+            className="cursor-pointer"
+          />
+          <img
+            src={icons.hr}
+            alt="Hrvatski"
+            onClick={() => switchLanguage("hr")}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
+      <h1>{t("header.welcome")}</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/assets.ts
@@ -0,0 +1,9 @@
+import logo from "../assets/icons/logo.png";
+import en from "../assets/icons/en.png";
+import hr from "../assets/icons/hr.png";
+
+export const icons = {
+    logo,
+    en,
+    hr,
+}
