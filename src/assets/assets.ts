9704af074a94eb16af1fc5475eaa136f12c28958
Dea <dea.bocina9@gmail.com>
2024-12-02 01:05:41 +0100

    Update UI design and styling

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,11 @@
 import logo from "../assets/icons/logo.png";
 import en from "../assets/icons/en.png";
 import hr from "../assets/icons/hr.png";
+import rightArrow from "../assets/icons/right-arrow.png";
 
 export const icons = {
-    logo,
-    en,
-    hr,
-}
+  logo,
+  en,
+  hr,
+  rightArrow,
+};
