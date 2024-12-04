4cb2cd62cfea221b8a70a209477f95ae390ad297
Dea <dea.bocina9@gmail.com>
2024-12-04 01:41:23 +0100

    Style project display and add necessary details (links, descriptions, etc.)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,40 +11,63 @@ const Projects = () => {
       description: t("projects.simplify.description"),
       technologies: t("projects.simplify.technologies"),
       keyFeatures: t("projects.simplify.keyFeatures"),
-      image: icons.showcaseSimplify,
       deployLink: "https://deabocina.github.io/simplify/",
     },
-    weather: {
-      name: t("projects.weather.name"),
+    weatherOrNot: {
+      name: t("projects.weatherOrNot.name"),
       logo: icons.logoWeather,
-      description: t("projects.weather.description"),
-      technologies: t("projects.weather.technologies"),
-      keyFeatures: t("projects.weather.keyFeatures"),
-      image: icons.showcaseWeather,
+      description: t("projects.weatherOrNot.description"),
+      technologies: t("projects.weatherOrNot.technologies"),
+      keyFeatures: t("projects.weatherOrNot.keyFeatures"),
       deployLink: "https://deabocina.github.io/weather-or-not/",
     },
   };
 
   return (
-    <>
-      {Object.entries(projects).map(([key, project]) => (
-        <div key={key} className="project">
-          <h1>{project.name}</h1>
-          <img src={project.logo} alt={project.name} />
-          <a
-            href={project.deployLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy Link
-          </a>
-          <p>{project.description}</p>
-          <p>{project.technologies}</p>
-          <p>{project.keyFeatures}</p>
-          <img src={project.image} alt={`${project.name} showcase`} />
-        </div>
-      ))}
-    </>
+    <div className="min-h-screen bg-gray-50 py-10 flex justify-center mb-10">
+      <div className="space-y-14">
+        {Object.entries(projects).map(([key, project]) => (
+          <div key={key} id={key} className="text-xl mt-12 px-4 max-w-3xl">
+            <div className="flex items-center mb-10 space-x-6 bg-white p-6 rounded-2xl shadow-md transition-all duration-300 ease-in-out hover:shadow-lg ">
+              <div className="w-20 h-20 p-3 bg-white rounded-full ring-4 ring-black transition-all duration-300 ease-in-out hover:ring-purple-600">
+                <a
+                  href={project.deployLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={project.logo}
+                    alt={project.name}
+                    className="object-cover w-full h-full"
+                  />
+                </a>
+              </div>
+
+              <div>
+                <h1 className="text-3xl font-medium text-gray-800 tracking-wide">
+                  {project.name}
+                </h1>
+                <small className="uppercase text-sm font-medium tracking-wider text-gray-400">
+                  {project.technologies}
+                </small>
+              </div>
+            </div>
+            <p className="text-gray-700">{project.description}</p>
+
+            <div className="mt-8 text-sm font-medium">
+              {project.keyFeatures.split(", ").map((tag, key) => (
+                <span
+                  key={key}
+                  className="inline-block mr-5 mb-2 bg-gray-200 rounded-3xl p-2 transition-all duration-300 ease-in-out hover:scale-110"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
